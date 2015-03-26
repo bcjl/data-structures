d6916034c923cc19971a1e7caf19f915758488b4
bcjl <briancjliu@gmail.com>
2015-03-26 15:49:13 -0700

    Completed linked list.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.tail === null){
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var output = list.head;
+    delete(list.head);
+    list.head = output.next;
+    return output.value;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while(currentNode !== null){
+      if(currentNode.value === target){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
