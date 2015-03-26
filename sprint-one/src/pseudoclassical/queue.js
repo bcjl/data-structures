6678c035bb2888d59ea77d95c8fc57080f39b12b
bcjl <briancjliu@gmail.com>
2015-03-25 20:02:02 -0700

    Completed queue for psuedoclassical pattern, regex test is still faulty.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.count = 0;
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
 };
 
+Queue.prototype.size = function(){
+  return this.count;
+};
 
+Queue.prototype.enqueue = function(input){
+  this.storage[this.last] = input;
+  this.last++;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function(){
+  var result = this.storage[this.first];
+  delete(this.storage[this.first]);
+  this.first++;
+  this.count = Math.max(0, this.count - 1);
+  return result;
+};
