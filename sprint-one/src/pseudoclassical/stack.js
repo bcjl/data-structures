62103d8933d3664ce851c4174b4e3be9b31db584
bcjl <briancjliu@gmail.com>
2015-03-25 19:57:28 -0700

    Completed stack for psuedoclassical pattern, faulty regex test failing

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function(){
+  return this.count;
+};
 
+Stack.prototype.pop = function(){
+  var result = this.storage[this.count - 1];
+  delete(this.storage[this.count - 1]);
+  this.count = Math.max(0, this.count - 1);
+  return result;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
