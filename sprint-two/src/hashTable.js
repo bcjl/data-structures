62e8aa99ba1a5846effa1613ff9b51dff6348d0e
bcjl <briancjliu@gmail.com>
2015-03-27 16:04:11 -0700

    Completed hashtables using arrays as buckets.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,38 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  if(!this._storage.get(i)){
+    var bucket = [];
+
+  } else{
+    var bucket = this._storage.get(i);
+  }
+  bucket.push([key, value]);
+  this._storage.set(i, bucket);
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+HashTable.prototype.retrieve = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(i);
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === key){
+      return bucket[j][1];
+    }
+  }
+  // this._storage.get(i)[1];
+  return null;
 };
 
-HashTable.prototype.remove = function(k){
-
+HashTable.prototype.remove = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(i);
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === key){
+      bucket.splice(j, 1);
+    }
+  };
 };
 
 
